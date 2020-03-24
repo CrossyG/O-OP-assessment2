251ae882bf531ff2487a5a0ea0e38a3aad780727
CrossyG <62619927+CrossyG@users.noreply.github.com>
2020-03-24 22:11:52 +0000

    Add files via upload

---
+++ Assessment 2 o-op/Assessment 2 o-op/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+
+namespace Assessment_2_o_op
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choose which files you want to test, 1,2,3");
+            var number = Console.ReadLine();
+
+            if (number == "1")
+            {
+                string file = File.ReadAllText("GitRepositories_1a.txt");
+                string file2 = File.ReadAllText("GitRepositories_1b.txt");
+            
+                if (file == file2)
+                {
+                    Console.WriteLine("they are not different");
+                }
+                else
+                {
+                    Console.WriteLine("They are different");
+                }
+            }
+            else if (number == "2")
+            {
+                string file = File.ReadAllText("GitRepositories_2a.txt");
+                string file2 = File.ReadAllText("GitRepositories_2b.txt");
+
+                if (file == file2)
+                {
+                    Console.WriteLine("they are not different");
+                }
+                else
+                {
+                    Console.WriteLine("They are different");
+                }
+            }
+            else
+            {
+                string file = File.ReadAllText("GitRepositories_3a.txt");
+                string file2 = File.ReadAllText("GitRepositories_3b.txt");
+
+                if (file == file2)
+                {
+                    Console.WriteLine("they are not different");
+                }
+                else
+                {
+                    Console.WriteLine("They are different");
+                }
+            }
+
+            
+        }
+    }
+}
