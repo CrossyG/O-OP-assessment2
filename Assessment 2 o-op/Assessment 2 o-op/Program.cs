1ba6681eb5f9433dde0eebb60c3ca86d995c1dff
CrossyG <62619927+CrossyG@users.noreply.github.com>
2020-03-25 16:46:33 +0000

    Add files via upload

--- Assessment 2 o-op/Assessment 2 o-op/Program.cs
+++ Assessment 2 o-op/Assessment 2 o-op/Program.cs
@@ -7,53 +7,81 @@ namespace Assessment_2_o_op
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Choose which files you want to test, 1,2,3");
+            Console.WriteLine("Choose which file you want to test, 1,2,3,4,5,6");
             var number = Console.ReadLine();
+
+            Console.WriteLine("Choose which file you want to test it against, 1,2,3,4,5,6");
+            var number2 = Console.ReadLine();
+
+            string file1 = File.ReadAllText("GitRepositories_1a.txt");
+            string file2 = File.ReadAllText("GitRepositories_1b.txt");
+            string file3 = File.ReadAllText("GitRepositories_2a.txt");
+            string file4 = File.ReadAllText("GitRepositories_2b.txt");
+            string file5 = File.ReadAllText("GitRepositories_3a.txt");
+            string file6 = File.ReadAllText("GitRepositories_3b.txt");
+
+            string choice = " ";
+            string choice2 = " ";
 
             if (number == "1")
             {
-                string file = File.ReadAllText("GitRepositories_1a.txt");
-                string file2 = File.ReadAllText("GitRepositories_1b.txt");
-            
-                if (file == file2)
-                {
-                    Console.WriteLine("they are not different");
-                }
-                else
-                {
-                    Console.WriteLine("They are different");
-                }
+                choice = file1;
+                
             }
             else if (number == "2")
             {
-                string file = File.ReadAllText("GitRepositories_2a.txt");
-                string file2 = File.ReadAllText("GitRepositories_2b.txt");
-
-                if (file == file2)
-                {
-                    Console.WriteLine("they are not different");
-                }
-                else
-                {
-                    Console.WriteLine("They are different");
-                }
+                choice = file2;
+            }
+            else if (number == "3")
+            {
+                choice = file3;
+            }
+            else if (number == "4")
+            {
+                choice = file4;
+            }
+            else if (number == "5")
+            {
+                choice = file5;
             }
             else
             {
-                string file = File.ReadAllText("GitRepositories_3a.txt");
-                string file2 = File.ReadAllText("GitRepositories_3b.txt");
-
-                if (file == file2)
-                {
-                    Console.WriteLine("they are not different");
-                }
-                else
-                {
-                    Console.WriteLine("They are different");
-                }
+                choice = file6;
             }
 
-            
+            if (number2 == "1")
+            {
+                choice2 = file1;
+            }
+            else if (number2 == "2")
+            {
+                choice2 = file2;
+            }
+            else if (number2 == "3")
+            {
+                choice2 = file3;
+            }
+            else if (number2 == "4")
+            {
+                choice2 = file4;
+            }
+            else if (number2 == "5")
+            {
+                choice2 = file5;
+            }
+            else 
+            {
+                choice2 = file6;
+            }
+
+            if (choice == choice2)
+            {
+                Console.WriteLine("They are the same");
+            }
+            else
+            {
+                Console.WriteLine("They are different");
+            }
         }
     }
 }
